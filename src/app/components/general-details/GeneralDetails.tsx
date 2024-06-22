d0c4b41e953b9b4cd4b18038af6e7be7d5efbb59
Akinleye Paul <paulakinleye@gmail.com>
2024-06-23 00:12:50 +0100

    add user-details header and tab

---
+++ src/app/components/general-details/GeneralDetails.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const GeneralDetails = () => {
+    return (
+        <>
+            <div className='details_container'>
+                <section>
+                    <p>Personal Information</p>
+                    <div>
+                        <span>
+                            <p>FULL NAME</p>
+                            <p>Grace Effiom</p>
+
+                        </span>
+                        <span>
+                            <p>Phone Number</p>
+                            <p>07060780922</p>
+
+                        </span>
+                        <span>
+                            <p>Email Address</p>
+                            <p>grace@gmail.com</p>
+
+                        </span>
+                        <span>
+                            <p>Bvn</p>
+                            <p>07060780922</p>
+                        </span>
+                        <span>
+                            <p>Gender</p>
+                            <p>Female</p>
+                        </span>
+
+                    </div>
+                    <div>
+                        <span>
+                            <p>FULL NAME</p>
+                            <p>Grace Effiom</p>
+
+                        </span>
+                        <span>
+                            <p>Phone Number</p>
+                            <p>07060780922</p>
+
+                        </span>
+                        <span>
+                            <p>Email Address</p>
+                            <p>grace@gmail.com</p>
+
+                        </span>
+                        <span>
+                            <p>Bvn</p>
+                            <p>07060780922</p>
+
+                        </span>
+                        <span>
+                            <p>Gender</p>
+                            <p>Female</p>
+
+                        </span>
+
+                    </div>
+                </section>
+            </div>
+
+
+        </>
+    )
+}
+
+export default GeneralDetails
--- src/app/user-details/page.tsx
+++ src/app/user-details/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import './user-details.scss'
 import Image from 'next/image'
@@ -5,10 +6,54 @@ import backButton from '../../../public/images/back-button.svg'
 import picture from '../../../public/images/profile-dp.svg'
 import star from '../../../public/images/start.svg'
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { useState } from 'react'
 
+import GeneralDetails from '../components/general-details/GeneralDetails'
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 
 const page = () => {
+
+
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+
+
   return (
     <>
       <div className='user_container'>
@@ -40,6 +85,49 @@ const page = () => {
               <p>9912345678/Providus Bank</p>
             </div>
           </div>
+
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider', }}>
+              <Tabs
+
+                sx={{
+                  "& .MuiTab-selected": {
+                    color: '#39CDCC',
+                    textDecorationColor: '#39CDCC'
+
+                  },
+
+                  //check it later
+
+
+                }}
+
+                value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab
+
+
+
+                  label="General Details" {...a11yProps(0)} />
+                <Tab label="Documents" {...a11yProps(1)} />
+                <Tab label="Bank Details" {...a11yProps(2)} />
+                <Tab label="Loans" {...a11yProps(3)} />
+                <Tab label="Savings" {...a11yProps(4)} />
+                <Tab label="App and System" {...a11yProps(5)} />
+
+              </Tabs>
+            </Box>
+          </Box>
+        </section>
+        <section>
+          <CustomTabPanel value={value} index={0}>
+            <GeneralDetails />
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            Item Three
+          </CustomTabPanel>
         </section>
       </div>
     </>
